b58bd620501d8e041ff81b2ec651ce0df8b469bf
Kabakcheevich <dkabakchei@edu.cdv.pl>
2026-04-11 13:23:08 +0200

    Colliders, boxes, and so on

--- Assets/ScriptsAll/Animations/GramophoneRotation.cs
+++ Assets/ScriptsAll/Animations/GramophoneRotation.cs
@@ -3,17 +3,20 @@ using UnityEngine;
 public class GramophoneRotation : MonoBehaviour
 {
     [SerializeField] private Transform plate;
-    [SerializeField] private Transform handle;
+    [SerializeField] private Transform handlePivot;
 
     [SerializeField] private float plateSpeed = 180f;
     [SerializeField] private float handleSpeed = 120f;
 
+    [SerializeField] private Vector3 plateAxis = Vector3.up;
+    [SerializeField] private Vector3 handleAxis = Vector3.right;
+
     private void Update()
     {
         if (plate != null)
-            plate.Rotate(Vector3.up * plateSpeed * Time.deltaTime);
+            plate.Rotate(plateAxis * plateSpeed * Time.deltaTime, Space.Self);
 
-        if (handle != null)
-            handle.Rotate(Vector3.right * handleSpeed * Time.deltaTime);
+        if (handlePivot != null)
+            handlePivot.Rotate(handleAxis * handleSpeed * Time.deltaTime, Space.Self);
     }
 }
